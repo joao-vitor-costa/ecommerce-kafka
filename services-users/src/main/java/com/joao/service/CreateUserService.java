66bdd47c68e7e8a235ef1b53875614b807865fe5
João Vitor Costa <dev.es.costa@gmail.com>
2020-08-14 01:13:43 -0300

    Update

--- services-users/src/main/java/com/joao/service/CreateUserService.java
+++ services-users/src/main/java/com/joao/service/CreateUserService.java
@@ -7,60 +7,61 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 public class CreateUserService {
 	
+	private final Connection connection;
 	
-	private final Connection conncetion;
-	
-	public CreateUserService() throws SQLException {
-		String url = "jdbc:sqlite:users_database.db";
-		this.conncetion = DriverManager.getConnection(url);
+	CreateUserService() throws SQLException {
+		String url = "jdbc:sqlite:target/users_database.db";
+		connection = DriverManager.getConnection(url);
 		try {
-			conncetion.createStatement().execute("create table Users(" +
+			connection.createStatement().execute("create table Users (" +
 					"uuid varchar(200) primary key," +
-					"email varchar (200))");
-		} catch (SQLException exception) {
-			// be careful, there sql could be wrong sql, be really careful
-			exception.printStackTrace();
+					"email varchar(200))");
+		} catch (SQLException ex) {
+			// be careful, the sql could be wrong, be reallllly careful
+			ex.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) throws SQLException {
 		var createUserService = new CreateUserService();
-		try (var service = new KafkaService(CreateUserService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
-				createUserService::parse, Order.class, Map.of())) {
+		try (var service = new KafkaService<>(CreateUserService.class.getSimpleName(),
+				"ECOMMERCE_NEW_ORDER",
+				createUserService::parse,
+				Order.class,
+				Map.of())) {
 			service.run();
 		}
 	}
 	
-	private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException,
-			SQLException {
-		System.out.println("-------------------------------------");
+	private void parse(ConsumerRecord<String, Order> record) throws SQLException {
+		System.out.println("------------------------------------------");
 		System.out.println("Processing new order, checking for new user");
 		System.out.println(record.value());
 		var order = record.value();
 		if (isNewUser(order.getEmail())) {
 			insertNewUser(order.getUserId(), order.getEmail());
 		}
-		
 	}
 	
 	private void insertNewUser(String uuid, String email) throws SQLException {
-		var insertUser = conncetion.prepareStatement("insert Users (uuid, email)" +
+		var insert = connection.prepareStatement("insert into Users (uuid, email) " +
 				"values (?,?)");
-		insertUser.setString(1, uuid);
-		insertUser.setString(2, email);
-		insertUser.execute();
-		System.out.println("User uuid e " + email + "auditioned");
-		
+		insert.setString(1, uuid);
+		insert.setString(2, email);
+		insert.execute();
+		System.out.println("Usuário uuid e " + email + " adicionado");
 	}
 	
 	private boolean isNewUser(String email) throws SQLException {
-		var exists = conncetion.prepareStatement("select uuid from Users where email = ? limit 1");
+		var exists = connection.prepareStatement("select uuid from Users " +
+				"where email = ? limit 1");
 		exists.setString(1, email);
 		var results = exists.executeQuery();
 		return !results.next();
 	}
+	
 }
+
